f8ab74ca8523c16214ef6c04c2cd4ce12ed5fa8b
alunity <75143943+alunity@users.noreply.github.com>
2025-06-14 03:13:55 +0100

    oops

--- scripts/setup.py
+++ scripts/setup.py
@@ -2,7 +2,7 @@ from subprocess import run
 from common import scripts_directory
 
 if __name__ == "__main__":
-    run(["python", "setupBackend"], cwd=scripts_directory)
+    run(["python", "setupBackend.py"], cwd=scripts_directory)
     # Order actually matters
     # The frontend setup generates types which depends on the backend venv
-    run(["python", "setupFrontend"], cwd=scripts_directory)
+    run(["python", "setupFrontend.py"], cwd=scripts_directory)
